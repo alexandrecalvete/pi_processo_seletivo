7d800a7d4545a8e6197a5826575f418ab9c87f61
Alexandre Calvete <alexandre.calvetee@gmail.com>
2017-11-29 11:27:35 -0200

    Fazendo o crud do controle e da view das tabelas

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -24,6 +24,51 @@ class AddressController extends Controller
      */
     public function index()
     {
-        return view('addresses/index');
+        $addresses = Addresses::all();
+        return view('addresses/index')->with('addresses', $addresses);
+    }
+
+    public function create()
+    {
+        return view('addresses/create');    
+    }
+
+    public function store(Request $request)
+    {
+
+        $address = new Address;
+        $address->nome = $request->input('nome');
+
+        if($address->save()) {
+          return redirect()->route('addresses.index')->with('success_message', 'Endereço cadastrado com sucesso.');
+        } else {
+          return redirect()->route('addresses.create')->with('error_message', 'Houve um erro ao cadastradar o endereço.');
+        }
+   }
+
+    public function edit(Request $request, $id)
+    {
+        $address = Address::findOrFail($id);
+        return view('addresses/edit')->with('address', $address);
+    }
+ 
+    public function update(Request $request, $id)
+    {
+        $address = ::findOrFail($id);
+        $address->nome = $request->input('nome');
+        if($address->save()) {
+            return redirect()->route('addresses.index')->with('success_message', 'Endereço alterado com sucesso.');
+        } else {
+            return redirect()->route('addresses.edit', $id)->with('error_message', 'Houve um erro ao alterar o endereço.');
+        }
+    }
+    public function destroy($id)
+    {
+        if (Address::destroy($id))
+            return redirect()->route('addresses.index')->with('success_message', 'Endereço deleto com sucesso.');
+        } else {
+            return redirect()->route('addresses.create')->with('error_message', 'Houve um erro ao deletar o endereço.');
+        }
     }
 }
+
--- app/Http/Controllers/SelectiveProcessesQuotaController.php
+++ app/Http/Controllers/SelectiveProcessesQuotaController.php
@@ -26,5 +26,52 @@ class SelectiveProcessesQuotaController extends Controller
     {
         $quotas = SelectiveProcessesQuota::all();
         return view('selectiveProcessesQuotas/index')->with('quotas', $quotas);
+            $addresses = Addresses::all();
+        return view('addresses/index')->with('addresses', $addresses);
+    }
+
+    public function create()
+    {
+        return view('profiles/create');    
+    }
+
+    public function store(Request $request)
+    {
+
+        $profile = new Profile;
+        $profile->nome = $request->input('nome');
+
+        if($profile->save()) {
+          return redirect()->route('profiles.index')->with('success_message', 'Perfil cadastrado com sucesso.');
+        } else {
+          return redirect()->route('profiles.create')->with('error_message', 'Houve um erro ao cadastradar o perfil.');
+        }
+   }
+
+    public function edit(Request $request, $id)
+    {
+        $profile = Profile::findOrFail($id);
+        return view('profiles/edit')->with('profile', $profile);
+    }
+ 
+    public function update(Request $request, $id)
+    {
+        $profile = ::findOrFail($id);
+        $profile->nome = $request->input('nome');
+        if($profile->save()) {
+            return redirect()->route('profiles.index')->with('success_message', 'Perfil alterado com sucesso.');
+        } else {
+            return redirect()->route('profiles.edit', $id)->with('error_message', 'Houve um erro ao alterar o perfil.');
+        }
+    }
+    public function destroy($id)
+    {
+        if (Profile::destroy($id))
+            return redirect()->route('profiles.index')->with('success_message', 'Perfil deleto com sucesso.');
+        } else {
+            return redirect()->route('profiles.create')->with('error_message', 'Houve um erro ao deletar o perfil.');
+        }
     }
 }
+
+
